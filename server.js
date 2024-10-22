2f0a73ef0f965f9909ae5333a5851e09b1a0e75c
Tim Zoltie <t.zoltie@gmail.com>
2024-10-22 14:32:01 +0100

    pokemon router created in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+const pokemonRouter = require("./route/pokemon.js")
 
 app.use(express.json())
+app.use('/pokemon', pokemonRouter)
 
 module.exports = app
